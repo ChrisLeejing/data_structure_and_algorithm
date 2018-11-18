fbda93df32916290ddddb179d76e4292d01b29e8
Chris <757561978@qq.com>
2018-11-18 20:01:27 +0800

    [add]: add the insertionSort.

--- src/com/jikeshijian/sorts11/Sorts.java
+++ src/com/jikeshijian/sorts11/Sorts.java
@@ -9,9 +9,10 @@ import java.util.Arrays;
  */
 public class Sorts {
     public static void main(String[] args) {
-        int[] arr = {3, 1, 5, 2, 4};
+        int[] arr = {3, 1, 5, 2, 7, 9, 6, 4, 8};
         int length = arr.length;
-        bubbleSort(arr, length);
+        // bubbleSort(arr, length);
+        insertionSort(arr, length);
         // Arrays.toString(arr): 源码中使用到了StringBuilder的拼接功能.
         System.out.println(Arrays.toString(arr));
     }
@@ -58,4 +59,43 @@ public class Sorts {
         }
     }
 
+    /**
+     * insertionSort: value from array[1] to array[n - 1], every time, compare the current value with the values before,
+     * if the front number is larger, move it 1 bit backward, or break the loop(the front number is smaller).
+     * then put the current value in the larger position.
+     * (remark: because the loop judgement j--, the larger position should plus 1)
+     * eg:
+     * 3, 1, 5, 2, 4
+     * 1, 3, 5, 2, 4    put 1 in the a[0] position.
+     * 1, 3, 5, 2, 4    break the inner loop.
+     * 1, 2, 3, 5, 4    put 2 between 1 and 3.
+     * 1, 2, 3, 4, 5    put 4 between 3 and 5.
+     * 1, 2, 3, 4, 5    the array is ok(the loop is over, the array is sorted ok.)
+     *
+     * @param a: the array to be sorted.
+     * @param n: the length of the array.
+     */
+    private static void insertionSort(int[] a, int n) {
+        if (n <= 1) {
+            return;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int value = a[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (a[j] > value) {
+                    a[j + 1] = a[j];
+                } else {
+                    break;
+                }
+            }
+            /*
+                put the current value into right position. now, a[j] is less than the value;
+                because the loop judgement j--, the larger position should plus 1.
+             */
+            a[j + 1] = value;
+
+        }
+    }
 }
